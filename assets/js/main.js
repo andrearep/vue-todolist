04fefd0b0e5a738dde937671511161f89ae96f72
andrearep <andrea.repe@gmail.com>
2021-05-19 15:58:40 +0200

    aggiunta possibilità di aggiungere ltask completate

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,7 @@ const app = new Vue({
     data: {
         todoList: [],
         recycleBin: [],
+        completedList: [],
         toDo: '',
         hide: false
     },
@@ -23,6 +24,12 @@ const app = new Vue({
         todoListToTrash: function todoListToTrash(index) {
             console.log(index);
             this.recycleBin.push(this.todoList[index])
+            this.todoList.splice(index, 1)
+        },
+
+        todoListToCOmplete: function todoListToCOmplete(index) {
+            console.log(index);
+            this.completedList.push(this.todoList[index])
             this.todoList.splice(index, 1)
         }
 
